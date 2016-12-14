f643f3b07a71562df0bec849056a4d9ee0052223
Paul Hammond <paul.hammond@bbc.co.uk>
2016-12-14 14:29:29 +0000

    can query the mongo db

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 'use strict';
 
 const Hapi = require('hapi');
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/threads';
+// Use connect method to connect to the Server 
+
+const findTopicByTopicName = (db, topicName, callback) => {
+    const collection = db.collection('threads');
+    collection.find({
+        name: topicName
+    }).toArray(function (err, docs) {
+        callback(docs);
+    });
+};
 
 // Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+server.connection({
+    host: 'localhost',
+    port: 8000
 });
 
 // Add the route
 server.route({
     method: 'GET',
-    path:'/topic/{topicName}', 
+    path: '/topic/{topicName}',
     handler: function (request, reply) {
-        return reply({test: encodeURIComponent(request.params.topicName)});
+        MongoClient.connect(url, function (err, db) {
+            findTopicByTopicName(db, request.params.topicName, (topic) => {
+                db.close();
+                return reply(topic);
+            });
+        });
+
     }
 });
 
